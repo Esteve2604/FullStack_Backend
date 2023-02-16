8eaeb5facae9f20079c7885c359db0023f9f859a
Esteve2604 <estebanasperuiz@gmail.com>
2023-02-16 13:43:57 +0100

    parte 4 avan

--- models/person.js
+++ models/person.js
@@ -2,16 +2,7 @@ require('dotenv').config()
 var uniqueValidator = require('mongoose-unique-validator');
 
 const mongoose = require('mongoose')
-console.log(process.env.MONGODB_URI)
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then( () => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+
 const noteSchema = new mongoose.Schema({
     name:{
       type: String, required: true, unique: true
